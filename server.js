1260d9ce8252da9430180abf7928469bfebce91a
Paul Sabandal <paulwithjames@gmail.com>
2019-08-19 02:49:00 +0800

    Modify ports

--- server.js
+++ server.js
@@ -76,7 +76,8 @@ app.get('/registration-referral', function(req, res) {
 
 app.use(express.static(__dirname));
 
-// Listen to port 5000
-app.listen(5000, function () {
-    console.log('Dev app listening on port 5000!');
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
 });
