f55306a986f61dbb6bc9775e9e9be3f36d72bc3d
Paul Sabandal <paulwithjames@gmail.com>
2019-08-19 01:13:15 +0800

    Add ejs and express, created partials

---
+++ server.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+const router = express.Router();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('pages/index.ejs');
+});
+
+app.get('/passenger', function(req, res) {
+    res.render('pages/passenger.ejs');
+});
+
+app.get('/rider', function(req, res) {
+    res.render('pages/rider.ejs');
+});
+
+app.get('/dashboard', function(req, res) {
+    res.render('pages/dashboard.ejs');
+});
+
+app.get('/dashboard-rider', function(req, res) {
+    res.render('pages/dashboard-rider.ejs');
+});
+
+app.get('/last-step', function(req, res) {
+    res.render('pages/last-step.ejs');
+});
+
+app.get('/login', function(req, res) {
+    res.render('pages/login.ejs');
+});
+
+app.get('/passenger', function(req, res) {
+    res.render('pages/passenger.ejs');
+});
+
+app.get('/privacy-policy', function(req, res) {
+    res.render('pages/privacy-policy.ejs');
+});
+
+app.get('/terms-for-passenger', function(req, res) {
+    res.render('pages/terms-for-passenger.ejs');
+});
+
+app.get('/terms-for-riders', function(req, res) {
+    res.render('pages/terms-for-riders.ejs');
+});
+
+app.get('/welcome', function(req, res) {
+    res.render('pages/welcome.ejs');
+});
+
+app.get('/success', function(req, res) {
+    res.render('pages/success.ejs');
+});
+
+app.get('/six-digit', function(req, res) {
+    res.render('pages/six-digit.ejs');
+});
+
+app.get('/faq', function(req, res) {
+    res.render('pages/faq.ejs');
+});
+
+app.get('/registration', function(req, res) {
+    res.render('pages/registration.ejs');
+});
+
+app.get('/registration-referral', function(req, res) {
+    res.render('pages/registration-referral.ejs');
+});
+
+app.use(express.static(__dirname));
+
+// Listen to port 5000
+app.listen(5000, function () {
+    console.log('Dev app listening on port 5000!');
+});
